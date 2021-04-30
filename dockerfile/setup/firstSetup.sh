bf79622bf20c85eb3d426454d2be59926d93bc7c
kokoro-labo <kokoro.aura@gmail.com>
2021-05-01 00:46:27 +0900

    add first setup sh

---
+++ dockerfile/setup/firstSetup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /home/oracle/.bashrc
+
+# First Oracle Setup
+echo "\n>>> first oracle setup"
+sqlplus / as sysdba << EOF
+    CONN /as SYSDBA;
+    SHOW CON_NAME;
+    ALTER USER SYS IDENTIFIED BY password;
+    ALTER SESSION SET CONTAINER=ORCLPDB1;
+    SHOW CON_NAME;
+    CREATE USER test IDENTIFIED BY password DEFAULT TABLESPACE USERS QUOTA UNLIMITED ON USERS TEMPORARY TABLESPACE TEMP;
+    GRANT DBA TO test;
+    GRANT UNLIMITED TABLESPACE TO test;
+    SELECT dtq.username, drp.granted_role, tablespace_name, dtq.bytes / 1024 / 1024 as TABLESPACE_MB FROM dba_role_privs drp, dba_ts_quotas dtq WHERE dtq.username = 'TEST' and drp.grantee = dtq.username;
+    exit;
+EOF
