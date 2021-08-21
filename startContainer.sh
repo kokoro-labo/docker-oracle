d656b0e770cafc3b7a59c9142ff957108cda38d8
kokoro-labo <kokoro.aura@gmail.com>
2021-08-21 15:28:05 +0900

    update: start and stop sh can choose container

--- startContainer.sh
+++ startContainer.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+if [ $# -ne 1 ];then
+    printf "\n>>> Please input container-name.\n"
+    exit 1
+fi
+
+cName=$1
+cService=`docker inspect --format '{{ index .Config.Labels "com.docker.compose.service"}}' "$cName"`
+
 printf "\n>>> start container\n"
-docker start ora19
+docker-compose start "$cService"
 sleep 5s
 
 # Start Listener and Oracle
-docker exec -u oracle ora19 /home/oracle/startOracle.sh
+docker exec -u oracle "$cName" /home/oracle/startOracle.sh
+
+exit 0
